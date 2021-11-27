0e3e5b76f86a2500672d833621160e0160664ceb
monika-ustjaniuk <monikaustjaniuk@hotmail.com>
2021-11-27 09:44:37 +0100

    funkcja-dodawanie

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let firstNumber = 10;
+let secondNumber = 20;
+
+function dodawanie(liczba1, liczba2)
+{
+let result = liczba1 + liczba2;
+return result;
+}
+
+console.log(dodawanie(firstNumber, secondNumber));
+
+console.log(dodawanie(3, secondNumber));
